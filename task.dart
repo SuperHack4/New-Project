2ee632b5b3b91090db41e85d491670cfbfb10e60
SuperHack4 <147961128+SuperHack4@users.noreply.github.com>
2025-03-12 22:12:38 +0200

    Add files via upload

---
+++ task.dart
@@ -0,0 +1,36 @@
+void main() async {
+  print("Add Task");
+  print(await addTask("Done"));
+
+  print("View");
+  print(await viewTasks());
+
+  print("Remove Task");
+  print(await removeTask("Done remove"));
+
+  print("Viewing Tasks");
+  print(await viewTasks());
+}
+
+List<String> tasks = [];
+
+Future<String> addTask(String task) async {
+  return await Future.delayed(Duration(seconds: 2), () {
+    tasks.add(task);
+    return "Task Added: $task";
+  });
+}
+
+Future<String> removeTask(String task) async {
+  return await Future.delayed(Duration(seconds: 2), () {
+    if (tasks.remove(task)) {
+      return "Task Removed: $task";
+    } else {
+      return "Task Not Found: $task";
+    }
+  });
+}
+
+Future<List<String>> viewTasks() async {
+  return await Future.delayed(Duration(seconds: 1), () => tasks);
+}
