dd655a83584b5565b4e6faa6d0e36cf8f5229329
SuperHack4 <147961128+SuperHack4@users.noreply.github.com>
2025-03-12 22:35:11 +0200

    Update task.dart

--- task.dart
+++ task.dart
@@ -1,6 +1,6 @@
 void main() async {
   print("Add Task");
-  print(await addTask("Done"));
+  print(await addTask("Task 1"));
 
   print("View");
   print(await viewTasks());
@@ -17,16 +17,16 @@ List<String> tasks = [];
 Future<String> addTask(String task) async {
   return await Future.delayed(Duration(seconds: 2), () {
     tasks.add(task);
-    return "Task Added: $task";
+    return "Task Added $task";
   });
 }
 
 Future<String> removeTask(String task) async {
   return await Future.delayed(Duration(seconds: 2), () {
     if (tasks.remove(task)) {
-      return "Task Removed: $task";
+      return "Task Removed $task";
     } else {
-      return "Task Not Found: $task";
+      return "You Not have a task $task";
     }
   });
 }
